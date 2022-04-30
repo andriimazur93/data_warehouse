3dfb77b4967abdc6912e1cbb8122104a2a5fdef6
andriimazur <andreymazur93@gmail.com>
2022-04-30 16:01:32 +0200

    adds stored procedure to create dim_employees table

---
+++ sql/stored_proc/load_dim_employees.sql
@@ -0,0 +1,42 @@
+create or alter procedure load_dim_employees
+AS
+Begin
+    Begin
+        insert into dim_employees (employee_id, first_name, last_name, email, phone_number, hire_date)
+        select emp.employee_id
+        , emp.first_name
+        , emp.last_name
+        , emp.email
+        , emp.phone_number
+        , emp.hire_date
+        from st_employees emp 
+        where not EXISTS(
+            select 1
+            from dim_employees dim_emp
+            where 
+            dim_emp.employee_id = emp.employee_id
+            and dim_emp.hire_date = emp.hire_date
+        )
+    End;
+    Begin
+        update dim_emp
+        set
+            dim_emp.first_name = emp.first_name
+            , dim_emp.last_name = emp.last_name
+            , dim_emp.email = emp.email
+            , dim_emp.phone_number = emp.phone_number
+            , dim_emp.updated_on = GETDATE()
+        from dim_employees dim_emp
+        join(
+            select emp.employee_id
+            , emp.first_name
+            , emp.last_name
+            , emp.email
+            , emp.phone_number
+            , emp.hire_date
+            from st_employees emp 
+        ) emp
+        on dim_emp.employee_id = emp.employee_id
+        and dim_emp.hire_date = emp.hire_date
+    End;
+End;
