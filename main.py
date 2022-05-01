dd3a8f6437b1b2a6d2b1fdd7c453bee3a2d13a85
andriimazur <andreymazur93@gmail.com>
2022-05-01 15:02:09 +0200

    implements incremental load

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from traceback import TracebackException
 from dao import get_source_connection, get_target_connection
 import pandas as pd
 import countries as con
@@ -7,6 +8,8 @@ import job_history
 import locations
 import jobs
 import regions
+from dw_job_run_summary import insert_into_job_run_summary
+from datetime import datetime
 
 
 target_tables = [
@@ -18,6 +21,7 @@ target_tables = [
     "regions",
     "jobs",
 ]
+
 
 def get_job_run_id():
     sql_query = "select max(job_run_id) from [DW].[dbo].[dw_job_run_summary]"
@@ -40,6 +44,36 @@ def get_job_run_id():
 
     return job_run_id
 
+def get_last_job_run_date(table_name):
+    sql_query = f"""
+        select cast(max(start_date_time) as date) as last_job_run_date
+        from dw_job_run_summary
+        where job_run_id in (
+            select max(job_run_id) 
+            from dw_job_run_summary
+            where tablename = '{table_name}'
+            and status = 'Success'
+        )
+        and tablename = '{table_name}'
+        """
+    print(sql_query)
+    cnxt = get_target_connection()
+    cursor = cnxt.cursor()
+    cursor.execute(sql_query)
+    
+    for row in cursor:
+        last_job_run_date = row[0]
+
+    if last_job_run_date is None:
+        last_job_run_date = '1900-01-01'
+
+    print(f"Value of last_job_run_date is {last_job_run_date}")
+
+    cursor.close()
+    cnxt.close()
+
+    return last_job_run_date
+
 
 def truncate_table(table_name):
     sql_query = f"truncate table dbo.st_{table_name}"
@@ -50,11 +84,16 @@ def truncate_table(table_name):
     cursor.close()
     cnxt.close()
 
-def fetch_records(table_name):
+def fetch_records(table_name, last_job_run_date):
     sql_query = f"""
         select *
         from {table_name}
+        where (
+            created_on >= DateAdd(dd, -3, '{last_job_run_date}')
+            or updated_on >= DateAdd(dd, -3, '{last_job_run_date}')
+        )
     """
+    print(sql_query)
     cnxn = get_source_connection()
     cursor = cnxn.cursor()
     cursor.execute(sql_query)
@@ -71,26 +110,38 @@ def fetch_records(table_name):
 
 
 def insert_records(table_name, job_run_id):
-    if table_name == 'countries':
-        con.insert_into_countries(table_name, job_run_id)
-    elif table_name == 'departments':
-        departments.insert_into_departments(table_name, job_run_id)
-    elif table_name == 'employees':
-        emp.insert_into_employees(table_name, job_run_id)
-    elif table_name == 'job_history':
-        job_history.insert_into_job_history(table_name, job_run_id)
-    elif table_name == 'locations':
-        locations.insert_into_locations(table_name, job_run_id)
-    elif table_name == 'jobs':
-        jobs.insert_into_jobs(table_name, job_run_id)
-    elif table_name == 'regions':
-        regions.insert_into_regions(table_name, job_run_id)
+    try:
+        df = pd.read_csv(f"{table_name}.csv")
+        
+        if table_name == 'countries':
+            con.insert_into_countries(df, table_name, job_run_id)
+        elif table_name == 'departments':
+            departments.insert_into_departments(df, table_name, job_run_id)
+        elif table_name == 'employees':
+            emp.insert_into_employees(df, table_name, job_run_id)
+        elif table_name == 'job_history':
+            job_history.insert_into_job_history(df, table_name, job_run_id)
+        elif table_name == 'locations':
+            locations.insert_into_locations(df, table_name, job_run_id)
+        elif table_name == 'jobs':
+            jobs.insert_into_jobs(df, table_name, job_run_id)
+        elif table_name == 'regions':
+            regions.insert_into_regions(df, table_name, job_run_id)
+    except Exception as e:
+        insert_into_job_run_summary(table_name, datetime.now(), end_time=datetime.now(), rows_processed=0, 
+                            status="Fail", error_message=str(e), col_id="", job_run_id=job_run_id)
 
 
 job_run_id = get_job_run_id()
+last_job_run_date = '2022-05-10'
 
 for table_name in target_tables:
-    fetch_records(table_name)
-    truncate_table(table_name)
-    insert_records(table_name, job_run_id)
-    print(table_name)
+    try:
+        last_job_run_date = get_last_job_run_date(table_name)
+        fetch_records(table_name, last_job_run_date)
+        truncate_table(table_name)
+        insert_records(table_name, job_run_id)
+        print(table_name)
+    except Exception as e:
+        print(e)
+
